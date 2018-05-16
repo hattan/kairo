33756f1ead9a995c2907c5b938678e80951f380f
Hattan Shobokshi <hattan@gmail.com>
2018-05-16 16:27:43 -0700

    added explicit env var name for slack and optional token for start_bot

--- kairo.py
+++ kairo.py
@@ -12,13 +12,14 @@ class Kairo:
 
     def __init__(self,name):
         self.name = name
-        self.load_token_from_env()
+        self.load_token_from_env(self.slack_token_name)
 
-    def load_token_from_env(self):
-        slack_token = os.environ.get(self.slack_token_name)
+    def load_token_from_env(self,name):
+        slack_token = os.environ.get(name)
         self.slack_token = slack_token
 
-    def start_bot(self):
-        if(self.slack_token is None):
+    def start_bot(self,token=None):
+        slack_token = token if (token is not None) else self.slack_token
+        if(slack_token is None):
             raise RuntimeError('missing slack token')
         return True
--- tests/test_kairo.py
+++ tests/test_kairo.py
@@ -28,3 +28,30 @@ def test_start_bot_throws_error_if_slack_token_not_in_environment_var():
         k.start_bot()
 
     assert 'missing slack token' in str(excinfo.value)
+
+@patch('os.environ.get')
+def test_Kairo_allows_to_specific_env_var_name(fake_env_get):
+    #arrange
+    fake_token_name = 'my_env_var'
+    
+    #act
+    k = Kairo("app")
+    k.load_token_from_env(name = fake_token_name)
+    k.start_bot()
+    
+    #assert
+    fake_env_get.assert_called_with(fake_token_name)
+
+def test_start_bot_takes_in_optional_slack_token():
+    #arrange
+    slack_token = "fake_slack_token"
+
+    #act
+    k = Kairo("app")
+    k.start_bot(slack_token)
+    
+    #assert
+    assert True # no failures
+
+
+
